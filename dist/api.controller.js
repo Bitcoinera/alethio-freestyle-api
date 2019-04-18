adcbf1f888ff8d56dcebd36cd757e7dbffa10c16
anagutjor <anagjordano@gmail.com>
2019-04-18 14:20:42 +0200

    update dist js files

--- dist/api.controller.js
+++ dist/api.controller.js
@@ -8,13 +8,119 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const request = require("request");
 function get_smart_contracts_by_transaction(req, res, next) {
     return __awaiter(this, void 0, void 0, function* () {
-        const hash = '0xe3a18b08b5fa88a63a5dfe686437f038e19af4e97b738f120888c6a782fa2249';
+        const txHash = req.headers.txhash;
         const base_url = process.env.BASE_URL;
-        //http.get(`${base_url}/transactions/${hash}/contractMessages`, res => { console.log(res)});
-        // res
-        //     .json( http.get(`${base_url}/transactions/${hash}/contractMessages`) )
+        request(`${base_url}/transactions/${txHash}/contractMessages`, (err, response, body) => {
+            res
+                .json({ statusCode: response.statusCode,
+                body: body });
+        });
     });
 }
 exports.get_smart_contracts_by_transaction = get_smart_contracts_by_transaction;
+function get_contract_message_log_entries(req, res, next) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const id = req.headers.id;
+        const base_url = process.env.BASE_URL;
+        request(`${base_url}/contract-messages/${id}/logEntries`, (err, response, body) => {
+            res
+                .json({ statusCode: response.statusCode,
+                body: body });
+        });
+    });
+}
+exports.get_contract_message_log_entries = get_contract_message_log_entries;
+// export async function get_contract_message_payload(req, res, next) {
+//     const id = req.headers.id;
+//     const base_url = process.env.BASE_URL;
+//     request(`${base_url}/contract-messages/${id}/msgPayload`, (err, response, body) => {
+//         res
+//             .json({ statusCode: response.statusCode,
+//                     body: body })
+//     })
+// }
+function get_account_transactions(req, res, next) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const account = req.headers.account;
+        const base_url = process.env.BASE_URL;
+        request(`${base_url}/accounts/${account}/transactions`, (err, response, body) => {
+            res
+                .json({ statusCode: response.statusCode,
+                body: body });
+        });
+    });
+}
+exports.get_account_transactions = get_account_transactions;
+function get_smart_contracts_by_account(req, res, next) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const account = req.headers.account;
+        const base_url = process.env.BASE_URL;
+        request(`${base_url}/accounts/${account}/contractMessages`, (err, response, body) => {
+            res
+                .json({ statusCode: response.statusCode,
+                body: body });
+        });
+    });
+}
+exports.get_smart_contracts_by_account = get_smart_contracts_by_account;
+// export async function get_transaction_message_payload(req, res, next) {
+//     const txHash = req.headers.txhash;
+//     const base_url = process.env.BASE_URL;
+//     request(`${base_url}/transactions/${txHash}/msgPayload`, (err, response, body) => {
+//         res
+//             .json({ statusCode: response.statusCode,
+//                     body: body })
+//     })
+// }
+function get_transaction_log_entries(req, res, next) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const txHash = req.headers.txhash;
+        const base_url = process.env.BASE_URL;
+        request(`${base_url}/transactions/${txHash}/logEntries`, (err, response, body) => {
+            res
+                .json({ statusCode: response.statusCode,
+                body: body });
+        });
+    });
+}
+exports.get_transaction_log_entries = get_transaction_log_entries;
+function get_contract_messages(req, res, next) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const contractAddress = req.headers.address;
+        const base_url = process.env.BASE_URL;
+        request(`${base_url}/contracts/${contractAddress}/contractMessages`, (err, response, body) => {
+            res
+                .json({ statusCode: response.statusCode,
+                body: body });
+        });
+    });
+}
+exports.get_contract_messages = get_contract_messages;
+function get_contract_log_entries(req, res, next) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const contractAddress = req.headers.address;
+        const base_url = process.env.BASE_URL;
+        request(`${base_url}/contracts/${contractAddress}/logEntries`, (err, response, body) => {
+            res
+                .json({ statusCode: response.statusCode,
+                body: body });
+        });
+    });
+}
+exports.get_contract_log_entries = get_contract_log_entries;
+function get_contract_transactions(req, res, next) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const contractAddress = req.headers.address;
+        const base_url = process.env.BASE_URL;
+        request(`${base_url}/contracts/${contractAddress}/transactions`, (err, response, body) => {
+            res
+                .json({ statusCode: response.statusCode,
+                body: body });
+        });
+    });
+}
+exports.get_contract_transactions = get_contract_transactions;
+// LogEntry.eventDecoded
--- dist/routes.js
+++ dist/routes.js
@@ -1,10 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
-const api_controller_1 = require("./api.controller");
+const ApiController = require("./api.controller");
 const routes = express_1.Router();
-routes.get('/explore-meta-transaction', api_controller_1.get_smart_contracts_by_transaction);
-routes.get('/get-account-activity');
-routes.get('/monitor-smart-contract');
-routes.get('/monitor-account');
+routes.get('/explore-meta-transaction', ApiController.get_smart_contracts_by_transaction);
+routes.get('/get-account-activity', ApiController.get_transaction_log_entries); // ApiController.get_smart_contracts_by_account  ApiController.get_account_transactions
+routes.get('/monitor-contract', ApiController.get_contract_transactions); // ApiController.get_contract_messages ApiController.get_contract_log_entries
+routes.get('/monitor-account', ApiController.get_account_transactions); // ApiController.get_smart_contracts_by_account
 exports.default = routes;
