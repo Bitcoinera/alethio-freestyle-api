5d1c4a499087d0461e085609543fcd59d2cf6718
anagutjor <anagjordano@gmail.com>
2019-04-23 19:43:32 +0200

    add toggle activities

--- app/app.ts
+++ app/app.ts
@@ -3,6 +3,7 @@ import * as AccountController from '../dist/account.controller.js';
 let input = (<HTMLInputElement>document.getElementById('account'));
 let responseUl = document.getElementById('response');
 let inputButton = document.getElementById('button');
+let toggleButton = document.getElementById('toggle-activities');
 let values;
 // account no contract msgs 0x84b50E8A5F0A3d28C10253d8B373882374c0FF89
 // account with con contract msgs 0x4Cf890695E2188a124495EbC3b1Ec6341F21C9CF
@@ -21,18 +22,33 @@ inputButton.addEventListener('click', async () => {
         values.contractMessages.forEach( (contractMessage, index) => {
             domString = domString + `
             <p>------> Activity Number ${index}</p>
-            <li>Type of action: ${contractMessage.action.type}</li>
-            <li>Block rank: ${contractMessage.action.blockRank}</li>
-            <li>Hash of the created contract: ${contractMessage.action.contractCreated}</li>
-            <li>Recipient address: ${contractMessage.action.toAddress}</li>
-            <li>Transaction hash: ${contractMessage.action.txHash}</li>
-            <li>Hash of the block where contract was created: ${contractMessage.action.inBlock}</li>
-            <li>Log entries: ${contractMessage.action.logEntries}</li>
+            <li class="activity">Type of action: ${contractMessage.action.type}</li>
+            <li class="activity">Block rank: ${contractMessage.action.blockRank}</li>
+            <li class="activity">Hash of the created contract: ${contractMessage.action.contractCreated}</li>
+            <li class="activity">Recipient address: ${contractMessage.action.toAddress}</li>
+            <li class="activity">Transaction hash: ${contractMessage.action.txHash}</li>
+            <li class="activity">Hash of the block where contract was created: ${contractMessage.action.inBlock}</li>
+            <li class="activity">Log entries: ${contractMessage.action.logEntries}</li>
             `;
         })
         responseUl.innerHTML = domString;
     }
    AccountController.monitor_account(input.value);
+})
+
+toggleButton.addEventListener('click', () => {
+    let lis = <HTMLElement[]><any>document.querySelectorAll('li.activity');
+    if ( lis.length > 0 ) {
+        lis.forEach( li => {
+            if ( li.style.display === 'none' ) {
+                li.style.display = 'block';
+            } else {
+                li.style.display = 'none';
+            }
+        })
+    } else {
+        console.log('No li elements to hide'); // No activities to hide
+    }
 })
 
 
