586ac069050c49da48d740dc50caadce5190eb6c
anagutjor <anagjordano@gmail.com>
2019-04-26 10:26:56 +0200

    compile with axios

--- dist/account.controller.d.ts
+++ dist/account.controller.d.ts
@@ -1,2 +1,1 @@
-export declare function monitor_account_req(req: any, res: any): Promise<void>;
 export declare function monitor_account(account: any): Promise<{}>;
--- dist/account.controller.js
+++ dist/account.controller.js
@@ -1,12 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const request = require("request");
-const base_url = 'https://api.aleth.io/v1';
-async function monitor_account_req(req, res) {
-    let account = req.headers.account;
-    res.json({ msg: 'Please use our UI at localhost:3001' });
-}
-exports.monitor_account_req = monitor_account_req;
+const axios_1 = require("axios");
+let url = `https://api.aleth.io/v1/accounts/`;
 async function monitor_account(account) {
     let response = {
         balance: '0',
@@ -28,46 +23,44 @@ async function monitor_account(account) {
         }
     };
     return new Promise((resolve, reject) => {
-        request(`${base_url}/accounts/${account}`, (err, res, body) => {
-            body = JSON.parse(body);
-            let balance = parseInt(body.data.attributes.balance) / 1000000000000000000;
+        axios_1.default.get(url + account)
+            .then(body => {
+            let balance = parseInt(body.data.data.attributes.balance) / 1000000000000000000;
             response.balance = balance.toFixed(4); // round up balance to 4 decimal places
-            request(`${base_url}/accounts/${account}/transactions`, (err, res, body) => {
-                body = JSON.parse(body);
-                response.numberTransactions = body.data.length.toString();
+            axios_1.default.get(url + account + '/transactions')
+                .then(body => {
+                response.numberTransactions = body.data.data.length.toString();
                 if (response.numberTransactions === '10') {
-                    response.numberTransactions = '10+  // this feature is a work in progress';
+                    response.numberTransactions = '10+  (this feature is a work in progress)';
                 }
-                request(`${base_url}/accounts/${account}/contractMessages`, (err, res, body) => {
-                    body = JSON.parse(body);
-                    if (body.data.length === 0) {
+                axios_1.default.get(url + account + '/contractMessages')
+                    .then(body => {
+                    if (body.data.data.length === 0) {
                         response.contractMessages = null;
                     }
                     else {
-                        for (let i = 0; i < body.data.length; i++) {
+                        for (let i = 0; i < body.data.data.length; i++) {
                             response.contractMessages[i] = {
                                 action: {
-                                    type: body.data[i].type,
-                                    blockRank: body.data[i].attributes.globalRank[1],
-                                    contractCreated: body.data[i].relationships.from.data.id,
-                                    inBlock: body.data[i].relationships.includedInBlock.data.id,
+                                    type: body.data.data[i].type,
+                                    blockRank: body.data.data[i].attributes.globalRank[1],
+                                    contractCreated: body.data.data[i].relationships.from.data.id,
+                                    inBlock: body.data.data[i].relationships.includedInBlock.data.id,
                                     logEntries: '',
-                                    toAddress: body.data[i].relationships.to.data.id,
-                                    txHash: body.data[i].relationships.transaction.data.id
+                                    toAddress: body.data.data[i].relationships.to.data.id,
+                                    txHash: body.data.data[i].relationships.transaction.data.id
                                 }
                             };
                         }
                         response.links = {
-                            next: body.links.next,
-                            prev: body.links.prev
+                            next: body.data.links.next,
+                            prev: body.data.links.prev
                         };
                     }
-                    resolve(response);
                 });
+                resolve(response);
             });
         });
     });
 }
 exports.monitor_account = monitor_account;
-const promise = monitor_account('0x4Cf890695E2188a124495EbC3b1Ec6341F21C9CF');
-promise.then((result) => { console.log(result), console.log(result['contractMessages']); }, (error) => { console.error(error); });
