cc1b2d229f731ba74f916484606410b57e37373f
anagutjor <anagjordano@gmail.com>
2019-04-29 15:28:30 +0200

    add individual toggles. add form

--- app/app.ts
+++ app/app.ts
@@ -1,16 +1,17 @@
 import * as AccountController from '../dist/account.controller.js';
 import * as ChartRenderer from '../dist/graphs.js'; 
 
+let form = document.getElementById("account-form");
 let input = (<HTMLInputElement>document.getElementById('account'));
 let divError = document.getElementById('error');
 let responseUl = document.getElementById('response');
-let inputButton = document.getElementById('button');
 
 // account with con contract msgs 0x4Cf890695E2188a124495EbC3b1Ec6341F21C9CF & another one 0x829bd824b016326a401d083b33d092293333a830
 // account with no contract msgs but txns 0xd73953bc13c031459f3856a9a5adce36bed18fdc
 // account with only 2 contract messages 0xb7605ddc0327406a7ac225b9de87865e22ac5927
 
-inputButton.addEventListener('click', async () => {
+form.addEventListener('submit', async (e) => {
+    e.preventDefault()
     // handle empty input
     if ( input.value === '' ) { return;}
     
@@ -66,12 +67,12 @@ inputButton.addEventListener('click', async () => {
                 `;
                 values['transactions'].forEach( (txn, index) => {
                     domString = domString + `
-                    <p>------> Transaction Number ${index}</p>
-                    <li class="txn">Hash: ${txn.txHash}</li> 
-                    <li class="txn">Block hash: ${txn.blockHash}</li> 
-                    <li class="txn">Gas limit: ${txn.gasLimit}</li> 
-                    <li class="txn">Gas price: ${txn.gasPrice}</li> 
-                    <li class="txn">Gas used: ${txn.gasUsed}</li> 
+                    <p><button class="txn t${index}">></button><span>Transaction ${index}</span></p>
+                    <li class="txn t${index}">Hash: ${txn.txHash}</li> 
+                    <li class="txn t${index}">Block hash: ${txn.blockHash}</li> 
+                    <li class="txn t${index}">Gas limit: ${txn.gasLimit}</li> 
+                    <li class="txn t${index}">Gas price: ${txn.gasPrice}</li> 
+                    <li class="txn t${index}">Gas used: ${txn.gasUsed}</li> 
                     `;
                 })
 
@@ -83,14 +84,13 @@ inputButton.addEventListener('click', async () => {
                 
                 values['contractMessages'].forEach( (contractMessage, index) => {
                     domString = domString + `
-                    <p>------> Activity Number ${index}</p>
-                    <li class="activity">Type of action: ${contractMessage.type}</li>
-                    <li class="activity">ETH Value: ${contractMessage.value}</li>
-                    <li class="activity">Block rank: ${contractMessage.blockRank}</li>
-                    <li class="activity">Hash of the contract: ${contractMessage.contractCreated}</li>
-                    <li class="activity">Recipient address: ${contractMessage.toAddress}</li>
-                    <li class="activity">Transaction hash: ${contractMessage.txHash}</li>
-                    <li class="activity">Block hash: ${contractMessage.blockHash}</li>
+                    <p><button class="activity a${index}">></button><span>Activity ${index}</span></p>
+                    <li class="activity a${index}">Type of action: ${contractMessage.type}</li>
+                    <li class="activity a${index}">Block rank: ${contractMessage.blockRank}</li>
+                    <li class="activity a${index}">Hash of the contract: ${contractMessage.contractCreated}</li>
+                    <li class="activity a${index}">Recipient address: ${contractMessage.toAddress}</li>
+                    <li class="activity a${index}">Transaction hash: ${contractMessage.txHash}</li>
+                    <li class="activity a${index}">Block hash: ${contractMessage.blockHash}</li>
                     `;
                 })
                 responseUl.innerHTML = domString;
@@ -125,6 +125,7 @@ const toggle = (type) => {
 }
 // event delegation model
 document.addEventListener('click',function(e){
+    let regex = new RegExp(/^txn|activity/);
     // toggle activities event
     if(e.target && e.target['id'] === 'toggle-transactions'){
         toggle('txn');
@@ -132,6 +133,11 @@ document.addEventListener('click',function(e){
     // toggle activities event
     else if(e.target && e.target['id'] === 'toggle-activities'){
         toggle('activity');
+    }
+    // toggle a single entry
+    else if(e.target && regex.test(e.target['className'])){
+        let type = e.target['className'].split(' ').join('.');
+        toggle(type);
     }
  });
 
